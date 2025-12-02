f0bdfb9f9d7aac42bce68dad0b1a29c3487e6552
bodoszidi <szidibee@gmail.com>
2025-12-02 20:22:47 +0000

    added the hard coded details

---
+++ src/Weather.js
@@ -0,0 +1,89 @@
+import "./Weather.css";
+import WeatherIcon from "./WeatherIcon";
+
+export default function Weather(props) {
+    let weatherData = {
+        city: "New York",
+        temperature: 19,
+        date: "Tuesday 10:00",
+        description: "Cloudy",
+        imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+        humidity: 80,
+        wind: 10,
+    };
+
+    return (
+        <div className="Weather">
+            <form className="mb-3">
+                <div className="row">
+                    <div className="col-9">
+                        <input
+                            type="search"
+                            placeholder="Type a city.."
+                            className="form-control"
+                            autoComplete="off"
+                        />
+                    </div>
+                    <div className="col-3">
+                        <input
+                            type="submit"
+                            value="Search"
+                            className="btn w-100"
+                        />
+                    </div>
+                </div>
+            </form>
+            <div className="row">
+                <div className="col-9 overview">
+                    <h1 className="py-4">{weatherData.city}</h1>
+                    <ul className="mx-2 py-2">
+                        <li className="py-2">
+                            Last updated: {weatherData.date}
+                        </li>
+                        <li>{weatherData.description}</li>
+                        <li>Humidity: {weatherData.humidity}%</li>
+                        <li>Wind: {weatherData.wind} km/h</li>
+                    </ul>
+                </div>
+                <div className="col-3">
+                    <div className="clearfix weather-temperature py-4">
+                        <img
+                            src={weatherData.imgUrl}
+                            alt={weatherData.description}
+                            className="float-left mx-2"
+                        />
+                        <div className="float-left">
+                            <strong>{weatherData.temperature}</strong>
+                            <span className="units">
+                                <a href="/">°C</a> | <a href="/">°F</a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row text-center mt-4">
+                <div className="col-3">
+                  <p>Monday</p>
+                    <WeatherIcon icon="CLOUDY" size={40} color="gray" />
+                    <p>14°5°</p>
+                </div>
+                <div className="col-3">
+                    <p>Tuesday</p>
+                    <WeatherIcon icon="RAIN" size={40} color="blue" />
+                    <p>14°5°</p>
+                </div>
+                <div className="col-3">
+                    <p>Wednesday</p>
+                    <WeatherIcon icon="SNOW" size={40} color="lightblue" />
+                    <p>14°5°</p>
+                </div>
+                <div className="col-3">
+                    <p>Thursday</p>
+                    <WeatherIcon icon="CLEAR_DAY" size={40} color="orange" />
+                    <p>14°5°</p>
+                </div>
+            </div>
+        </div>
+    );
+}
